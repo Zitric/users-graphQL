e489a155e785024b773c41347914eac258cbed3f
Zitric <zitricwebdeveloper@gmail.com>
2019-06-26 14:17:25 +0100

    Initial commit

---
+++ schema/schema.js
@@ -0,0 +1,15 @@
+const graphql = require('graphql');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    }
+})
