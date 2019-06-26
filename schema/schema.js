b29c9b443c5fa79d909fb90ad19fb8bc5c713da2
Zitric <zitricwebdeveloper@gmail.com>
2019-06-26 19:01:32 +0100

    Updating

--- schema/schema.js
+++ schema/schema.js
@@ -1,15 +1,52 @@
 const graphql = require('graphql');
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
+
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        company: { 
+            type: CompanyType,
+            resolve( parentValue, args ) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then( res => res.data);
+            }
+        }
     }
-})
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString }},
+            resolve( parentValue, args ) {
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then( response => response.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
